bd1f5d7fe9d52307d807db23228b592e783c041e
Yazmin Perez <zoeperez333@gmail.com>
2022-08-07 21:19:09 -0700

    update notes and fix formatting

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 // Use a counter to keep track of the length of a substring.
 // Return object.
 
+// ["aabb", "aaaa", "bbab"]
+// ['aabb', 'bbab']
+// ['aabb', 'bbab', "aaaa"]
+
 longestSubstring = (arr) => {
   let maxCount = 0;
   let runCount = 1;
@@ -15,12 +19,12 @@ longestSubstring = (arr) => {
     const permute = (arg, m = []) => {
       // arg: placeholder for the input
       if (arg.length === 0) {
-        result.push(m); // m holds the elements -> as it goes through the loop it removes an element until it hits 0
+        result.push(m); // m holds the elements -> as it goes through the loop it removes an element
       } else {
         for (let i = 0; i < arg.length; i++) {
           let curr = arg.slice();
-          let next = curr.splice(i, 1);
-          permute(curr.slice(), m.concat(next)); // passes a copy of curr, concats next into m -> pushes it into the end of the array -> repeats the loop until it hits 0
+          let next = curr.splice(i, 1); // remove 1 element at current index
+          permute(curr.slice(), m.concat(next)); // passes a copy of curr, concats next into m -> pushes it into the end of the array -> repeats the loop
         }
       }
     };
